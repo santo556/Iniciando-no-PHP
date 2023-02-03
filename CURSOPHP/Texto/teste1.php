3453d9418a085f7d7c4473e0413f6fac7483d9ed
santo556 <72739630+santo556@users.noreply.github.com>
2023-02-02 22:15:07 -0400

    Tudo Atualizado.

---
+++ CURSOPHP/Texto/teste1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T</title>
+</head>
+<body>
+    <h1>EXEMPLO PHP</h1>
+    <?php 
+        $nome = "André Venâncio dos Santos";
+        echo "<p>O meu nome é $nome</p>";
+        echo "<p>Estou aprendendo PHP</p>";
+    ?>
+    <h2>Minha idade</h2>
+    <?php
+        $idade = 41;
+        echo "<p>Minha idade é $idade</p>";
+    ?>
+</body>
+</html>
