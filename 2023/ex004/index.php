228272e429a8187f94d5687ddbe1c47eb700c0b3
santo556 <72739630+santo556@users.noreply.github.com>
2023-03-01 19:21:12 -0400

    Interagindo com Formulários.

--- 2023/ex004/index.php
+++ 2023/ex004/index.php
@@ -64,6 +64,7 @@
         $canal = "Curso em Video";
         $ano = date('Y');
         echo <<< 'TESTE'
+                Olá Novo Mundo!
             Olá galera do $canal!
                     </br>Tudo bem com vocês?
                 </br>Como está sendo esse ano de $ano?
